1d8449814c9f952b801c0404844996cb8166167c
Sven Dahlstrand <sven@dahlstrand.net>
2018-01-09 14:43:47 +0100

    Add "Are you prepared?" section draft.

--- 10-print.asm
+++ 10-print.asm
@@ -11,6 +11,15 @@
 ;
 ; > `10 PRINT` running on Commodore 64 (left) and Game Boy (right).
 ;
+; Are you prepared?
+; -----------------
+;
+; https://www.mankier.com/7/rgbds
+; Game Boy programmer manual
+;
+; https://skilldrick.github.io/easy6502/
+; https://schweigi.github.io/assembler-simulator/
+;
 ; A-MAZE-ING
 ; ----------
 ;
